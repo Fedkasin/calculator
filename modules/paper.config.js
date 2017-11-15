3e38b09c6cf358393c9e9e2e9c411ace157b965d
federalisimo <federalisimo1@gmail.com>
2017-11-15 21:12:06 +0300

    Products implementation started

--- modules/paper.config.js
+++ modules/paper.config.js
@@ -1,3 +1,131 @@
+var products = {
+  posters: {
+    name: 'Афиши',
+    cover: false,
+    insideBlock: false
+  },
+  tickets: {
+    name: 'Билеты',
+    cover: false,
+    insideBlock: false
+  },
+  blanks: {
+    name: 'Бланки',
+    cover: false,
+    insideBlock: false
+  },
+  brochures: {
+    name: 'Брошюры',
+    cover: true,
+    insideBlock: true
+  },
+  journals: {
+    name: 'Журналы',
+    cover: true,
+    insideBlock: true
+  },
+  catalogs: {
+    name: 'Каталоги',
+    cover: true,
+    insideBlock: true
+  },
+  notepads: {
+    name: 'Блокноты',
+    cover: true,
+    insideBlock: true
+  },
+  booklets: {
+    name: 'Буклеты',
+    cover: false,
+    insideBlock: false
+  },
+  cards: {
+    name: 'Визитки',
+    cover: false,
+    insideBlock: false
+  },
+  voblers: {
+    name: 'Воблеры',
+    cover: false,
+    insideBlock: false
+  },
+  warrantyCoupons: {
+    name: 'Гарантийные талоны',
+    cover: false,
+    insideBlock: false
+  },
+  dyplomas: {
+    name: 'Дипломы и грамоты',
+    cover: false,
+    insideBlock: false
+  },
+  discountCards: {
+    name: 'Дисконтные карты',
+    cover: false,
+    insideBlock: false
+  },
+  calendars: {
+    name: 'Календари',
+    cover: false,
+    insideBlock: false
+  },
+  leaflets: {
+    name: 'Листовки',
+    cover: false,
+    insideBlock: false
+  },
+  menus: {
+    name: 'Меню',
+    cover: false,
+    insideBlock: false
+  },
+  stickers: {
+    name: 'Наклейки и стикеры',
+    cover: false,
+    insideBlock: false
+  },
+  postcards: {
+    name: 'Открытки',
+    cover: false,
+    insideBlock: false
+  },
+  invitations: {
+    name: 'Приглашения',
+    cover: false,
+    insideBlock: false
+  },
+  giftSertificates: {
+    name: 'Подарочные сертификаты',
+    cover: false,
+    insideBlock: false
+  },
+  tableTockers: {
+    name: 'Тейблтокеры',
+    cover: false,
+    insideBlock: false
+  },
+  fliers: {
+    name: 'Флаеры',
+    cover: false,
+    insideBlock: false
+  },
+  prices: {
+    name: 'Ценники, бирки и ярлыки',
+    cover: false,
+    insideBlock: false
+  },
+  hangers: {
+    name: 'Хенгеры',
+    cover: false,
+    insideBlock: false
+  },
+  shelfTockers: {
+    name: 'Шелфтокеры',
+    cover: false,
+    insideBlock: false
+  }
+};
+
 var formats = {
   print: ['Односторонняя 4+0', 'Двухсторонняя 4+4'],
   paper: ['A3', 'A4', 'A5', 'A6']
@@ -15,4 +143,4 @@ var lamination = {
 var a3Size = 312*440;
 
 angular.module('paper-config', [])
-.constant('paperConfig', { formats, types, a3Size, lamination});
+.constant('paperConfig', { products, formats, types, a3Size, lamination});
